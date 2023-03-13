bcf79f92f426b8cee5f1276f7a2f60c71542fa0a
방서빈 <newmso5382@gmail.com>
2023-03-13 17:24:50 +0900

    초기세팅

---
+++ frontend/pumpkinmarket/src/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Nav from "./components/Nav/Nav";
+import Chatting from "./pages/Chatting/Chatting";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import Posting from "./pages/Posting/Posting";
+import Product from "./pages/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
+import SignUp from "./pages/SignUp/SignUp";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/nav" element={<Nav />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/posting" element={<Posting />} />
+        <Route path="/chatting" element={<Chatting />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ frontend/pumpkinmarket/src/pages/Login/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="text-3xl font-bold underline text-gray-500">Login</div>
+  );
+}
