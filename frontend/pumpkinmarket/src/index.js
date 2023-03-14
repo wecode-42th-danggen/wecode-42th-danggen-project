a074056dd56b6d517e29ab8ce83c856705b5b94d
방서빈 <newmso5382@gmail.com>
2023-03-14 16:38:16 +0900

    useref 추가 & react 에러 해결

--- frontend/pumpkinmarket/src/index.js
+++ frontend/pumpkinmarket/src/index.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import './index.css';
--- frontend/pumpkinmarket/src/pages/Posting/Posting.js
+++ frontend/pumpkinmarket/src/pages/Posting/Posting.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export default function Posting() {
   return <div>Posting</div>;
