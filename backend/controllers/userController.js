c3c54ce391cc8414bd3becb9e07980957f359cfe
이홍열 <zeler1004@gmail.com>
2023-03-15 14:32:35 +0900

    🔥[backend][이홍열] signin 기능구현(자체 로그인)  - jwt 방식 토큰 발행

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,8 +2,7 @@ const userService = require('../services/userService');
 const { catchAsync } = require('../utils/error');
 
 const signUp = catchAsync(async (req, res) => {
-  const { socialId, email, password, phoneNumber, nickName, profileImageUrl } =
-    req.body;
+  const { email, password, phoneNumber, nickName } = req.body;
 
   if (!email || !password || !phoneNumber || !nickName) {
     const error = new Error('KEY_ERROR');
@@ -11,15 +10,24 @@ const signUp = catchAsync(async (req, res) => {
     throw error;
   }
 
-  await userService.signUp(
-    socialId,
-    email,
-    password,
-    phoneNumber,
-    nickName,
-    profileImageUrl
-  );
+  await userService.signUp(email, password, phoneNumber, nickName);
   return res.status(201).json({ message: 'SUCCESS_CREATE_USER' });
 });
 
-module.exports = { signUp };
+const signIn = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    const error = new Error('KEY_ERROR');
+    error.statusCode = 400;
+    throw error;
+  }
+
+  const accessToken = await userService.signIn(email, password);
+
+  return res
+    .status(200)
+    .json({ message: `SUCCESS_LOG_IN, TOKEN_NUMBER : ${accessToken}` });
+});
+
+module.exports = { signUp, signIn };
--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,6 +1,6 @@
 const checkValidationEmail = async (email) => {
   const EMAIL_REGEX = new RegExp(
-    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*[@]{1}[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*[.]{1}[a-zA-Z]{2,3}$/i
   );
   if (!EMAIL_REGEX.test(email)) {
     const err = new Error('INVALID_EMAIL');
@@ -11,7 +11,7 @@ const checkValidationEmail = async (email) => {
 
 const checkValidationPassword = async (password) => {
   const PASSWORD_REGEX = new RegExp(
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%^&*()_+|~\-=?{}[\];:'",.<>/])[A-Za-z\d@$!%^&*()_+|~\-=?{}[\];:'",.<>/]{8,20}$/
   );
   if (!PASSWORD_REGEX.test(password)) {
     const err = new Error('INVALID_PASSWORD');
@@ -22,7 +22,7 @@ const checkValidationPassword = async (password) => {
 
 const checkValdationPhoneNumber = async (phoneNumber) => {
   const PHONE_NUMBER_REGEX = new RegExp(
-    '^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})'
+    /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})/
   );
   if (!PHONE_NUMBER_REGEX.test(phoneNumber)) {
     const err = new Error('INVALID_PHONE_NUMBER');
