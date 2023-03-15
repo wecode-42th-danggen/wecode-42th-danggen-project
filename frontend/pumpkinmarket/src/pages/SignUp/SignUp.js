ff7b282cd723bb7af9178dc339a524181c0ec0d4
방서빈 <newmso5382@gmail.com>
2023-03-15 14:53:47 +0900

    ADD:useref 추가

--- frontend/pumpkinmarket/src/pages/SignUp/SignUp.js
+++ frontend/pumpkinmarket/src/pages/SignUp/SignUp.js
@@ -1,41 +1,58 @@
-import React, { useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function SignUp() {
-  const [imgFile, setImgFile] = useState('');
-  const imgRef = useRef();
+  useEffect(() => {
+    fetch(`../../../public/data/test.json`, {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  }, []);
 
-  const saveImgFile = () => {
-    const file = imgRef.current.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setImgFile(reader.result);
-    };
+  const [image, setImage] = useState({
+    image_file: '',
+    preview_URL: 'img/default_image.png',
+  });
+
+  const navigate = useNavigate();
+  let inputRef;
+
+  function signUpSumbit() {
+    navigate('/login');
+  }
+
+  const saveImage = e => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      // 새로운 이미지를 올리면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+      URL.revokeObjectURL(image.preview_URL);
+      const preview_URL = URL.createObjectURL(e.target.files[0]);
+      setImage(() => ({
+        image_file: e.target.files[0],
+        preview_URL: preview_URL,
+      }));
+    }
   };
+
   return (
-    <div className="flex justify-center ">
-      <h1> signUp</h1>
+    <div className="flex justify-center">
       <div className="flex justify-center leading-10 h-screen align-items: center;">
         <form
-          className="flex flex-col space-x-6 justify-center items-center "
+          className="flex flex-col space-x-6 justify-center items-center"
           encType="multipart/form-data"
           method="POST"
           action="login"
         >
-          <div class="shrink-0">
-            <img
-              className="h-16 w-16 object-cover rounded-full"
-              src={
-                imgFile
-                  ? imgFile
-                  : `https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80`
-              }
-              alt="Current profile photo"
-            />
+          <div className="img-wrapper flex w-full h-24 max-w-xs overflow-hidden">
+            <img src={image.preview_URL} className="rounded-full" />
           </div>
-          <span class="sr-only">Choose profile photo</span>
           <label for="avatar">Avatar</label>
           <input
+            multiple
             type="file"
             class="block w-full text-sm text-slate-500
       file:mr-4 file:py-2 file:px-4
@@ -47,10 +64,12 @@ export default function SignUp() {
             name="avatar"
             accept="image/*"
             id="profileImg"
-            onChange={saveImgFile}
-            ref={imgRef}
+            onChange={saveImage}
+            onClick={e => (e.target.value = null)}
+            ref={refParam => (inputRef = refParam)}
+            // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
+            // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
           />
-
           <input
             placeholder="email"
             type="email"
@@ -60,7 +79,7 @@ export default function SignUp() {
           <input placeholder="id" type="id" name="id"></input>
           <input placeholder="password" type="password" name="password"></input>
           <input placeholder="phone" type="text" name="phone_number"></input>
-          <input type="submit" value="Update Profile"></input>
+          <input type="submit" value="update" onClick="signUpSumbit"></input>
         </form>
       </div>
     </div>
