6ff139f71a320d192afa909e0c3195f5e6f1adf9
방서빈 <newmso5382@gmail.com>
2023-03-16 12:08:00 +0900

    수정

--- frontend/pumpkinmarket/src/pages/Login/Login.js
+++ frontend/pumpkinmarket/src/pages/Login/Login.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-export default function Login() {
+export default function SignUp() {
+  const [formData, setFormData] = useState({ id: '', password: '' });
   return (
-    <div className="text-3xl font-bold underline text-gray-500">Login</div>
+    <div className="flex justify-center ">
+      <div className="flex justify-center leading-10 h-screen align-items: center;">
+        <form
+          className="flex flex-col space-x-6 justify-center items-center "
+          encType="multipart/form-data"
+          method="POST"
+          action="login"
+        >
+          <input
+            placeholder="email"
+            type="email"
+            name="profile_image_url"
+          ></input>
+          <input placeholder="password" type="password" name="password"></input>
+          <input type="submit" value="제출"></input>
+        </form>
+      </div>
+    </div>
   );
 }
