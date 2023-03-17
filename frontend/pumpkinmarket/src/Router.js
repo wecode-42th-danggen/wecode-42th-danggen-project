6102f1f2458452d7aae89bb8dd8b606b1dfc2ae4
soomminglee <soomming.lee@gmail.com>
2023-03-17 12:13:15 +0900

    라우터 수정

--- frontend/pumpkinmarket/src/Router.js
+++ frontend/pumpkinmarket/src/Router.js
@@ -6,6 +6,9 @@ import Chat from './pages/Chatting/Chat';
 import Login from './pages/Login/Login';
 import Main from './pages/Main/Main';
 import Mypage from './pages/Mypage/Mypage';
+import NeighborInfo from './pages/NeighborInfo/NeighborInfo';
+import NeighborInfoList from './pages/NeighborInfoList/NeighborInfoList';
+import NeighborInfoPosting from './pages/NeighborInfoPosting/NeighborInfoPosting';
 import Posting from './pages/Posting/Posting';
 import Product from './pages/Product/Product';
 import ProductList from './pages/ProductList/ProductList';
@@ -24,6 +27,9 @@ const Router = () => {
         <Route path="/chat" element={<Chat />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/neighborinfo" element={<NeighborInfo />} />
+        <Route path="/neighborinfo-list" element={<NeighborInfoList />} />
+        <Route path="/neighborinfo-posting" element={<NeighborInfoPosting />} />
       </Routes>
       <Footer />
     </BrowserRouter>
