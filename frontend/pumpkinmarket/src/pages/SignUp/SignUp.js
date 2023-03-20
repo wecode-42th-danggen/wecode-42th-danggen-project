40cb54fb8e00073de3bbfdb229fbf893bc7a902b
방서빈 <newmso5382@gmail.com>
2023-03-20 11:03:52 +0900

    Modify: signup에 login 한 것 삭제

--- frontend/pumpkinmarket/src/pages/SignUp/SignUp.js
+++ frontend/pumpkinmarket/src/pages/SignUp/SignUp.js
@@ -1,6 +1,5 @@
 import { React, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import SignUp from '../Login/Login';
 
 function SignUpForm() {
   const [signupInfo, setSignupInfo] = useState('');
