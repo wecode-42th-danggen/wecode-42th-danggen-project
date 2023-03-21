83320fa70d40cae439107d92b8dec558c7e17408
방서빈 <newmso5382@gmail.com>
2023-03-21 15:25:07 +0900

    Add: 줄간격추가

--- frontend/pumpkinmarket/src/pages/AdminProject/AdminProject.js
+++ frontend/pumpkinmarket/src/pages/AdminProject/AdminProject.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function AdminProject() {
   return (
-    <div className="pt-20 h-screen">
+    <div className="pt-20 h-screen leading-7">
       <ul>
         <li>
           <Link to="/member">회원관리</Link>
