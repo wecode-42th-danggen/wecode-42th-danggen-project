e4daa65995516441992606fead57a865d1afdd9d
방서빈 <newmso5382@gmail.com>
2023-03-21 15:06:50 +0900

    admin 경로 삭제

--- frontend/pumpkinmarket/src/Router.js
+++ frontend/pumpkinmarket/src/Router.js
@@ -13,7 +13,6 @@ import Posting from './pages/Posting/Posting';
 import Product from './pages/Product/Product';
 import ProductList from './pages/ProductList/ProductList';
 import SignUp from './pages/SignUp/SignUp';
-import Admin from './pages/Admin/Admin';
 import AdminProject from './pages/AdminProject/AdminProject';
 
 const Router = () => {
@@ -33,7 +32,6 @@ const Router = () => {
         <Route path="/neighborinfo-list" element={<NeighborInfoList />} />
         <Route path="/neighborinfo-posting" element={<NeighborInfoPosting />} />
         <Route path="/admin-project" element={<AdminProject />} />
-        <Route path="/admin" element={<Admin />} />
       </Routes>
       <Footer />
     </BrowserRouter>
