9bc9cb339eeb997efbb1e7e024dbb4c61a7156c9
juseong <jjs7280@gmail.com>
2023-03-24 14:36:16 +0900

    [backend 장주성] add: 마이페이지 조회 기능 추가

---
+++ backend/controllers/myPageController.js
@@ -0,0 +1,45 @@
+const myPageService = require('../services/myPageService');
+const { catchAsync } = require('../utils/error');
+
+const getUserPostsByUserId = catchAsync(async (req, res) => {
+  const userId = req.user;
+
+  const data = await myPageService.getUserPostsByUserId(userId);
+  res.status(200).json({ data });
+});
+
+const getUserLikesByUserId = catchAsync(async (req, res) => {
+  const userId = req.user;
+
+  const data = await myPageService.getUserLikesByUserId(userId);
+  res.status(200).json({ data });
+});
+
+const getCommunityPostsByUserId = catchAsync(async (req, res) => {
+  const userId = req.user;
+
+  const data = await myPageService.getCommunityPostsByUserId(userId);
+  res.status(200).json({ data });
+});
+
+const getCommunityCommentsByUserId = catchAsync(async (req, res) => {
+  const userId = req.user;
+
+  const data = await myPageService.getCommunityCommentsByUserId(userId);
+  res.status(200).json({ data });
+});
+
+const getChatsByUserId = catchAsync(async (req, res) => {
+  const userId = req.user;
+
+  const data = await myPageService.getChatsByUserId(userId);
+  res.status(200).json({ data });
+});
+
+module.exports = {
+  getUserPostsByUserId,
+  getUserLikesByUserId,
+  getCommunityPostsByUserId,
+  getCommunityCommentsByUserId,
+  getChatsByUserId,
+};
---
+++ backend/services/myPageService.js
@@ -0,0 +1,29 @@
+const myPageDao = require('../models/myPageDao');
+
+const getUserPostsByUserId = async (userId) => {
+  return await myPageDao.getUserPostsByUserId(userId);
+};
+
+const getUserLikesByUserId = async (userId) => {
+  return await myPageDao.getUserLikesByUserId(userId);
+};
+
+const getCommunityPostsByUserId = async (userId) => {
+  return await myPageDao.getCommunityPostsByUserId(userId);
+};
+
+const getCommunityCommentsByUserId = async (userId) => {
+  return await myPageDao.getCommunityCommentsByUserId(userId);
+};
+
+const getChatsByUserId = async (userId) => {
+  return await myPageDao.getChatsByUserId(userId);
+};
+
+module.exports = {
+  getUserPostsByUserId,
+  getUserLikesByUserId,
+  getCommunityPostsByUserId,
+  getCommunityCommentsByUserId,
+  getChatsByUserId,
+};
