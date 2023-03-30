e4718e8a5a5c5b1f30002c2f127c07ea305c7401
soomminglee <soomming.lee@gmail.com>
2023-03-30 18:03:32 +0900

    maypage 반응형 구현 완료

--- frontend/pumpkinmarket/src/pages/Mypage/ChattingInventory.js
+++ frontend/pumpkinmarket/src/pages/Mypage/ChattingInventory.js
@@ -5,8 +5,8 @@ import { API } from '../../config/config';
 export default function ChattingInventory() {
   const [chattingInventory, setChattingInventory] = useState([]);
   const [deleteModal, setDeletModal] = useState(false);
-  console.log('chattingInventory::', chattingInventory);
-  // const Token = localStorage.getItem('accessToken');
+
+  const Token = localStorage.getItem('accessToken');
 
   const deleteBtn = () => {
     setDeletModal(prev => !prev);
@@ -17,8 +17,7 @@ export default function ChattingInventory() {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
-        authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE5LCJpc3MiOiJ3YWVtLWRhbmdnZW4iLCJpYXQiOjE2Nzk1ODYxMjksImV4cCI6MTQ1MTE2MjQxNTQ1NjAwfQ.uy_nFfR9sJH1g9Hr-m9pJh0rIjxbUOl3iC6eLv4ZRdo',
+        authorization: Token,
       },
     })
       .then(res => res.json())
@@ -28,84 +27,84 @@ export default function ChattingInventory() {
   }, []);
 
   return (
-    <>
-      <div className="flex justify-center min-h-[36rem]">
-        {chattingInventory.length === 0 ? (
-          <div className="flex justify-center items-center pt-10 max-md:h-screen">
-            <img
-              className="w-7"
-              src="/images/Mypage/noposting.png"
-              alt="no search product"
-            />
-            <div>채팅 기록이 없습니다.</div>
-          </div>
-        ) : (
-          <ul>
-            {chattingInventory.map(list => {
-              return (
-                <div className="flex" key={list.roomId}>
-                  <Link to={`${process.env.PUBLIC_URL}/product/${list.postId}`}>
-                    <li className="flex justify-between">
-                      <div>
-                        <hr className="w-[44rem]" />
-                        <div className="flex items-center justify-between p-2">
-                          <div className="flex items-center">
-                            <p className="mr-3 text-sm font-bold">
-                              💌 {list.roomId}
-                            </p>
-                            <img
-                              className="w-10 h-10 rounded-full mr-2"
-                              src={list.buyerImage}
-                              alt="chatting user profileImg"
-                            />
-                            <div className="flex flex-col ml-3">
-                              <span className="text-sm font-bold pb-2 w-full">
-                                {list.buyerNickname}
-                              </span>
-                              <span className="w-full">{list.title}</span>
-                            </div>
-                          </div>
-                          <img
-                            className="w-20 h-20 rounded-lg object-cover mr-8 max-md:hidden"
-                            src={list.imageUrl}
-                            alt="chatting title img"
-                          />
-                        </div>
-                      </div>
-                    </li>
-                  </Link>
-                  <button type="button" onClick={deleteBtn}>
-                    ×
-                  </button>
-                </div>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      {deleteModal && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 z-1 bg-gray-100/75">
-          <div className="bg-white w-64 h-32 rounded-lg flex flex-col justify-center items-center top-1/2 left-1/2 absolute -translate-y-1/2 -translate-x-1/2">
-            <p className="pb-5">게시글을 삭제 하시겠습니까?</p>
-            <div>
-              <button
-                type="button"
-                className="bg-slate-200 w-16 h-8 rounded mr-5 hover:bg-slate-300"
-                onClick={deleteBtn}
-              >
-                취소
-              </button>
-              <button
-                type="button"
-                className="bg-green-500 w-16 h-8 rounded hover:bg-green-600"
-                // onClick={() => handleDeletBtn(commentInventory[0].postId)}
-              >
-                삭제
-              </button>
-            </div>
-          </div>
+    <div className="flex justify-center max-md:min-h-[28rem]">
+      {chattingInventory.length === 0 ? (
+        <div className="flex justify-center items-center pt-10">
+          <img
+            className="w-7"
+            src="/images/Mypage/noposting.png"
+            alt="no search product"
+          />
+          <div>채팅 기록이 없습니다.</div>
         </div>
+      ) : (
+        <ul>
+          {chattingInventory.map(list => {
+            return (
+              <div
+                className="flex border-t border-solid border-slate-200 max-md:w-[40rem]"
+                key={list.roomId}
+              >
+                <Link to={`${process.env.PUBLIC_URL}/product/${list.postId}`}>
+                  <li className="flex justify-between">
+                    <div>
+                      <div className="flex items-center justify-between p-2">
+                        <div className="flex items-center">
+                          <p className="mr-3 text-sm font-bold">
+                            💌 {list.roomId}
+                          </p>
+                          <img
+                            className="w-10 h-10 rounded-full mr-2"
+                            src={list.buyerImage}
+                            alt="chatting user profileImg"
+                          />
+                          <div className="flex flex-col ml-3">
+                            <span className="text-sm font-bold pb-2 w-full">
+                              {list.buyerNickname}
+                            </span>
+                            <span className="w-full">{list.title}</span>
+                          </div>
+                        </div>
+                        <img
+                          className="w-20 h-20 rounded-lg object-cover mr-8 max-md:hidden"
+                          src={list.imageUrl}
+                          alt="chatting title img"
+                        />
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+                <button type="button" onClick={deleteBtn}>
+                  ×
+                </button>
+                {deleteModal && (
+                  <div className="fixed top-0 left-0 right-0 bottom-0 z-1 bg-gray-100/75">
+                    <div className="bg-white w-64 h-32 rounded-lg flex flex-col justify-center items-center top-1/2 left-1/2 absolute -translate-y-1/2 -translate-x-1/2">
+                      <p className="pb-5">게시글을 삭제 하시겠습니까?</p>
+                      <div>
+                        <button
+                          type="button"
+                          className="bg-slate-200 w-16 h-8 rounded mr-5 hover:bg-slate-300"
+                          onClick={deleteBtn}
+                        >
+                          취소
+                        </button>
+                        <button
+                          type="button"
+                          className="bg-green-500 w-16 h-8 rounded hover:bg-green-600"
+                          // onClick={() => handleDeletBtn(list.postId)}
+                        >
+                          삭제
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </ul>
       )}
-    </>
+    </div>
   );
 }
--- frontend/pumpkinmarket/src/pages/Mypage/DealingsInventory.js
+++ frontend/pumpkinmarket/src/pages/Mypage/DealingsInventory.js
@@ -5,7 +5,7 @@ import { API } from '../../config/config';
 export default function DealingsInventory() {
   const [dealingsInventory, setDealingsInventory] = useState([]);
   const [deleteModal, setDeletModal] = useState(false);
-
+  console.log('dealingsInventory::', dealingsInventory);
   const Token = localStorage.getItem('accessToken');
 
   const deleteBtn = () => {
@@ -52,86 +52,84 @@ export default function DealingsInventory() {
   }, []);
 
   return (
-    <>
-      <div className="flex justify-center min-h-[32rem]">
-        {dealingsInventory.length === 0 ? (
-          <div className="flex justify-center items-center pt-10">
-            <img
-              className="w-7"
-              src="/images/Mypage/noposting.png"
-              alt="no search product"
-            />
-            <div>등록한 게시글이 없습니다.</div>
-          </div>
-        ) : (
-          <ul>
-            {dealingsInventory.map(list => {
-              return (
-                <div
-                  className="flex border-t border-solid border-slate-200 p-5"
-                  key={list.postId}
-                >
-                  <Link to={`${process.env.PUBLIC_URL}/product/${list.postId}`}>
-                    <li className="flex justify-between">
-                      <div>
-                        <h1 className="font-bold pb-2 pl-2 text-lg">
-                          {list.postTitle}
-                        </h1>
-                        <div className="flex items-center">
-                          <p className="text-ellipsis overflow-hidden break-words line-clamp-2 w-[44rem] h-12 pl-2 mr-8">
-                            {list.postDescription}
-                          </p>
-                          <img
-                            className="w-20 h-20 rounded-lg object-cover mr-8 max-md:hidden"
-                            src={list.postImageUrl}
-                            alt="inventory title img"
-                          />
-                        </div>
-                        <div className="flex items-center">
-                          <img
-                            className="w-7 h-7 rounded-full mr-3"
-                            src={list.profileImageUrl}
-                            alt="user profile img"
-                          />
-                          <p className="font-semibold text-gray-600">
-                            {list.userNickname}
-                          </p>
-                        </div>
-                      </div>
-                    </li>
-                  </Link>
-                  <button type="button" onClick={deleteBtn}>
-                    ×
-                  </button>
-                </div>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      {deleteModal && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 z-1 bg-gray-100/75">
-          <div className="bg-white w-64 h-32 rounded-lg flex flex-col justify-center items-center top-1/2 left-1/2 absolute -translate-y-1/2 -translate-x-1/2">
-            <p className="pb-5">게시글을 삭제 하시겠습니까?</p>
-            <div>
-              <button
-                type="button"
-                className="bg-slate-200 w-16 h-8 rounded mr-5 hover:bg-slate-300"
-                onClick={deleteBtn}
-              >
-                취소
-              </button>
-              <button
-                type="button"
-                className="bg-green-500 w-16 h-8 rounded hover:bg-green-600"
-                onClick={() => handleDeletBtn(dealingsInventory[0].postId)}
-              >
-                삭제
-              </button>
-            </div>
-          </div>
+    <div className="flex justify-center max-md:min-h-[28rem]">
+      {dealingsInventory.length === 0 ? (
+        <div className="flex justify-center items-center pt-10">
+          <img
+            className="w-7"
+            src="/images/Mypage/noposting.png"
+            alt="no search product"
+          />
+          <div>등록한 게시글이 없습니다.</div>
         </div>
+      ) : (
+        <ul>
+          {dealingsInventory.map(list => {
+            return (
+              <div
+                className="flex border-t border-solid border-slate-200 p-5 max-md:w-[40rem]"
+                key={list.postId}
+              >
+                <Link to={`${process.env.PUBLIC_URL}/product/${list.postId}`}>
+                  <li className="flex justify-between">
+                    <div>
+                      <h1 className="font-bold pb-2 pl-2 text-lg">
+                        {list.postTitle}
+                      </h1>
+                      <div className="flex items-center">
+                        <p className="text-ellipsis overflow-hidden break-words line-clamp-2 w-[44rem] h-12 pl-2 mr-8 max-md:w-[36rem]">
+                          {list.postDescription}
+                        </p>
+                        <img
+                          className="w-20 h-20 rounded-lg object-cover mr-8 max-md:hidden"
+                          src={list.postImageUrl}
+                          alt="inventory title img"
+                        />
+                      </div>
+                      <div className="flex items-center max-md:pt-3">
+                        <img
+                          className="w-7 h-7 rounded-full mr-3"
+                          src={list.profileImageUrl}
+                          alt="user profile img"
+                        />
+                        <p className="font-semibold text-gray-600">
+                          {list.userNickname}
+                        </p>
+                      </div>
+                    </div>
+                  </li>
+                </Link>
+                <button type="button" onClick={deleteBtn}>
+                  ×
+                </button>
+                {deleteModal && (
+                  <div className="fixed top-0 left-0 right-0 bottom-0 z-1 bg-gray-100/75">
+                    <div className="bg-white w-64 h-32 rounded-lg flex flex-col justify-center items-center top-1/2 left-1/2 absolute -translate-y-1/2 -translate-x-1/2">
+                      <p className="pb-5">게시글을 삭제 하시겠습니까?</p>
+                      <div>
+                        <button
+                          type="button"
+                          className="bg-slate-200 w-16 h-8 rounded mr-5 hover:bg-slate-300"
+                          onClick={deleteBtn}
+                        >
+                          취소
+                        </button>
+                        <button
+                          type="button"
+                          className="bg-green-500 w-16 h-8 rounded hover:bg-green-600"
+                          onClick={() => handleDeletBtn(list.postId)}
+                        >
+                          삭제
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </ul>
       )}
-    </>
+    </div>
   );
 }
