d2c95491b5413c071e6492d93208ae613b81d351
soomminglee <soomming.lee@gmail.com>
2023-03-31 11:52:07 +0900

    동네정보리스트페이지 레이아웃

---
+++ frontend/pumpkinmarket/src/pages/NeighborInfoList/NeighborIncident.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NeighborIncident() {
+  return <div>NeighborIncident</div>;
+}
---
+++ frontend/pumpkinmarket/src/pages/NeighborInfoList/NeighborInformation.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NeighborInformation() {
+  return <div>NeighborInformation</div>;
+}
